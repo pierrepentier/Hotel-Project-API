51fdf92587883f7f594ff8e024fa6a90ca1ffba1
pierre <pentier.pierre@yahoo.fr>
2020-04-30 17:59:57 +0200

    ajout ApiResource sur Entity 'Hotel'

--- src/Entity/Hotel.php
+++ src/Entity/Hotel.php
@@ -2,11 +2,11 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass="App\Repository\HotelRepository")
  */
 class Hotel
